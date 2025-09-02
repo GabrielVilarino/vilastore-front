e3663aaec5ed945226bf6434d6df03a206e89077
GabrielVilarino <gabriel_vilarino@hotmail.com>
2025-09-01 22:21:35 -0300

    [FEAT] Add url production

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:8000/:path*'
+                // destination: 'http://localhost:8000/:path*'
+                destination: `${API_URL}/:path*`
             }
         ]
     }
